0fc17072fcb958be1b38559caeb872a136ab7320
Us Nickolay <mr.us.nick@gmail.com>
2020-01-13 20:30:14 +0300

    Initial commit

---
+++ src/AsciiCharSequence.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+public static class AsciiCharSequence  {
+    byte[] a;
+    CharSequence b = new String("USSR");
+    System.out.println("");
+    sout
+
+
+    public AsciiCharSequence(byte[] a) {
+        this.a = a;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+    public static void main(String[] args){
+       String name = "admin";
+       String char1 = name.substring(0,1); //char="a"
+        System.out.println(char1);
+        char character = 'a';
+        int ascii ;
+        for (int i =0 ; i<100 ; i++)
+        System.out.println();
+        System.out.println();
+    }
+}
